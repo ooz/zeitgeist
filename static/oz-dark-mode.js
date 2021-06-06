d2cc556bb7f4ff79ea9281480c5059333d20a924
ooz <ooz@users.noreply.github.com>
2021-06-06 20:47:50 +0200

    Remove unneeded static files

--- static/oz-dark-mode.js
+++
@@ -1,3 +0,0 @@
-function toggleTheme() { document.body.classList.toggle("dark-mode") }
-function initTheme() { let h=new Date().getHours(); if (h <= 8 || h >= 20) { toggleTheme() } }
-
--- static/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget -q https://raw.githubusercontent.com/ooz/templates/master/html/oz.css -O oz.css
-wget -q https://raw.githubusercontent.com/ooz/templates/master/html/oz-dark-mode.js -O oz-dark-mode.js
-
